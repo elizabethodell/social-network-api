472259907ee0f82de1afab2c9d977f1d01cad932
elizabethodell <89113028+elizabethodell@users.noreply.github.com>
2021-12-29 14:30:40 -0500

    Update user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    deleteFriend
+} = require('../../controllers/user-controller');
 
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router.route('/:userId/friends/:friendId')
+    .delete(deleteFriend)
+
+module.exports = router;
